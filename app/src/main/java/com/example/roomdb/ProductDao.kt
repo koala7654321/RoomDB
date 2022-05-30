ed41c877c46b7d6a1dfd522fce609a37a1a69b14
ChiaChia <s1081808@gm.pu.edu.tw>
2022-05-30 22:18:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roomdb/ProductDao.kt
@@ -0,0 +1,19 @@
+package com.example.roomdb
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface ProductDao {
+
+    @Insert
+    fun insertProd( pro: Product)
+    @Query("select * from products where proName= :pro")
+    fun findProd(pro: String): List<Product>
+    @Query("delete from products where proName =:pro")
+    fun delProd( pro: String)
+    @Query("select * from products")
+    fun getALL(): List<Product>
+
+}
